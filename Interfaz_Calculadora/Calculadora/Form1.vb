a09c51f2153a17e2c139359c9a4a3e9a43a604f3
Devion <madara098javier@gmail.com>
2022-10-02 15:17:58 -0500

    Initial commit

---
+++ Interfaz_Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmPrincipal
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnuno.Click
+        'Todo se ejecutara al hacer click'
+
+    End Sub
+End Class
